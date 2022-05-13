128b4194c0ed3691fa27fb38d63f691bb5b09377
Orkhan Abbasov <webskyus@gmail.com>
2022-05-13 07:19:13 +0400

    start findIndex func create, step 1 done

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 import './polyfill/index.js'
-import fill from "./source/fill.js";
+import findIndex from "./source/findIndex.js";
 
-console.log('fill', fill([4, 6, 8, 10], '*', 2))
+/*
+✅ _.findIndex(users, function(o) { return o.user == 'barney'; });
+// => 0
 
+// The `_.matches` iteratee shorthand.
+_.findIndex(users, { 'user': 'fred', 'active': false });
+// => 1
+
+// The `_.matchesProperty` iteratee shorthand.
+_.findIndex(users, ['active', false]);
+// => 0
+
+// The `_.property` iteratee shorthand.
+_.findIndex(users, 'active');
+// => 2
+* */
+
+const users = [
+  { 'user': 'barney',  'active': false },
+  { 'user': 'fred',    'active': false },
+  { 'user': 'pebbles', 'active': true }
+];
+
+console.log(
+  findIndex(users, (item) => {
+    return item.user === 'barney'
+  })
+)
